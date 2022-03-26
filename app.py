a036c703ab3e0aae19ebc9456e3e3f8e332a144b
wwha2121 <wwha2121@gmail.com>
2022-03-27 00:15:45 +0900

    modalDataToserver

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://wwha2121:enemy12!@cluster0.mu5ys.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-db = client.macNewland
+client = MongoClient('mongodb+srv://wwha2121:enemy12!@newland.mxgol.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+db = client.newland
 
 @app.route('/')
 def home():
@@ -16,7 +16,18 @@ def home():
 
 @app.route("/mapMakingHome", methods=["POST"])
 def mapMakingHome_post():
-    print("d")
+    family1_receive = request.form['family1_give']
+    family2_receive = request.form['family2_give']
+    family3_receive = request.form['family3_give']
+    family4_receive = request.form['family4_give']
+
+    doc = {
+        'family1': family1_receive,
+        'family2': family2_receive,
+        'family3': family3_receive,
+        'family4': family4_receive
+    }
+    db.guestbookDb.insert_one(doc)
 
 
 
@@ -35,7 +46,7 @@ def homework_post():
         'name': name_receive,
         'comment': comment_receive
     }
-    db.guestbookDb.insert_one(doc)
+    db.familyDb.insert_one(doc)
 
     return jsonify({'msg': '저장완료!'})
 
